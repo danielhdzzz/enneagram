d1527011909de207c37db0376db4bdf80f174a6f
danielhdzzz <dhdzch@gmail.com>
2019-02-26 20:13:17 +0100

    clearer way to draw lines

--- build/js/main.js
+++ build/js/main.js
@@ -1,113 +1,111 @@
 'use strict';
 
 var enneagram = function enneagram(settings) {
-	var canvas = document.getElementById(settings.targetId);
-	var ctx = canvas.getContext('2d');
-	var canvasW = getCanvasW();
-	var opt = {
-		radius: canvas.width / 3,
-		maxColor: [255, 10, 10],
-		minColor: [230, 230, 230],
-		lineWidth: 2,
-		lineColor: 'black',
-		pointSize: 15,
-		pointFontSize: 17,
-		fontColor: 'black'
-	};
-	var center_x = canvas.width / 2;
-	var center_y = canvas.height / 2;
-	var angleIncr = 360 / 9;
+    var canvas = document.getElementById(settings.targetId);
+    var ctx = canvas.getContext('2d');
+    var canvasW = getCanvasW();
+    var opt = {
+        radius: canvas.width / 3,
+        maxColor: [255, 10, 10],
+        minColor: [230, 230, 230],
+        lineWidth: 2,
+        lineColor: 'black',
+        pointSize: 15,
+        pointFontSize: 17,
+        fontColor: 'black'
+    };
+    var center_x = canvas.width / 2;
+    var center_y = canvas.height / 2;
+    var angleIncr = 360 / 9;
 
-	// override if options are set
-	for (var key in settings.options) {
-		if (settings.options.hasOwnProperty(key)) {
-			opt[key] = settings.options[key];
-		}
-	}
+    // override if options are set
+    for (var key in settings.options) {
+        if (settings.options.hasOwnProperty(key)) {
+            opt[key] = settings.options[key];
+        }
+    }
 
-	function getCanvasW() {
-		canvas.style.width = '100%';
-		canvas.width = canvas.offsetWidth;
-		canvas.style.height = canvas.width + 'px';
-		canvas.height = canvas.offsetHeight;
-		return canvas.width;
-	}
+    function getCanvasW() {
+        canvas.style.width = '100%';
+        canvas.width = canvas.offsetWidth;
+        canvas.style.height = canvas.width + 'px';
+        canvas.height = canvas.offsetHeight;
+        return canvas.width;
+    }
 
-	var resize = function resize() {
-		canvasW = getCanvasW();
-		opt.radius = canvasW / 3;
-		center_x = canvasW / 2;
-		center_y = canvasW / 2;
-	};
+    var resize = function resize() {
+        canvasW = getCanvasW();
+        opt.radius = canvasW / 3;
+        center_x = canvasW / 2;
+        center_y = canvasW / 2;
+    };
 
-	// draw
-	var draw = function draw() {
-		var drawCircle = function drawCircle() {
-			ctx.lineWidth = opt.lineWidth;
-			ctx.beginPath();
-			ctx.arc(center_x, center_y, opt.radius, 0, 2 * Math.PI);
-			ctx.stroke();
-		};
+    // draw
+    var draw = function draw() {
+        var drawCircle = function drawCircle() {
+            ctx.lineWidth = opt.lineWidth;
+            ctx.beginPath();
+            ctx.arc(center_x, center_y, opt.radius, 0, 2 * Math.PI);
+            ctx.stroke();
+        };
 
-		var drawPoint = function drawPoint(angle, label, percentage) {
-			var x = center_x + opt.radius * Math.cos(-angle * Math.PI / 180);
-			var y = center_y + opt.radius * Math.sin(-angle * Math.PI / 180);
+        var drawPoint = function drawPoint(angle, label, percentage) {
+            var x = center_x + opt.radius * Math.cos(-angle * Math.PI / 180);
+            var y = center_y + opt.radius * Math.sin(-angle * Math.PI / 180);
 
-			ctx.fillStyle = interpolateColor(percentage);
-			ctx.beginPath();
-			ctx.arc(x, y, opt.pointSize, 0, 2 * Math.PI);
-			ctx.fill();
-			ctx.fillStyle = opt.fontColor;
-			ctx.font = opt.pointFontSize + 'px Arial';
-			ctx.fillText(label, x - opt.pointFontSize / 4, y + opt.pointFontSize / 3);
-		};
+            ctx.fillStyle = interpolateColor(percentage);
+            ctx.beginPath();
+            ctx.arc(x, y, opt.pointSize, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.fillStyle = opt.fontColor;
+            ctx.font = opt.pointFontSize + 'px Arial';
+            ctx.fillText(label, x - opt.pointFontSize / 4, y + opt.pointFontSize / 3);
+        };
 
-		var drawLine = function drawLine(angle1, angle2) {
-			var x1 = center_x + opt.radius * Math.cos(-angle1 * Math.PI / 180);
-			var y1 = center_y + opt.radius * Math.sin(-angle1 * Math.PI / 180);
-			var x2 = center_x + opt.radius * Math.cos(-angle2 * Math.PI / 180);
-			var y2 = center_y + opt.radius * Math.sin(-angle2 * Math.PI / 180);
-			ctx.strokeStyle = opt.lineColor;
-			ctx.lineWidth = opt.lineWidth;
-			ctx.beginPath();
-			ctx.moveTo(x1, y1);
-			ctx.lineTo(x2, y2);
-			ctx.stroke();
-		};
+        var drawLine = function drawLine(angle1, angle2) {
+            var x1 = center_x + opt.radius * Math.cos(-angle1 * Math.PI / 180);
+            var y1 = center_y + opt.radius * Math.sin(-angle1 * Math.PI / 180);
+            var x2 = center_x + opt.radius * Math.cos(-angle2 * Math.PI / 180);
+            var y2 = center_y + opt.radius * Math.sin(-angle2 * Math.PI / 180);
+            ctx.strokeStyle = opt.lineColor;
+            ctx.lineWidth = opt.lineWidth;
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+        };
 
-		var interpolateColor = function interpolateColor(percentage) {
-			var rgbVals = [];
-			for (var i = 0; i < 3; i++) {
-				var perc = percentage;
-				var top = opt.maxColor[i];
-				var bottom = opt.minColor[i];
-				var distance = top - bottom;
-				var position = bottom + perc / 100 * distance;
-				rgbVals.push(position);
-			}
-			var result = 'rgb(' + rgbVals[0] + ',' + rgbVals[1] + ',' + rgbVals[2] + ')';
-			return result;
-		};
+        var interpolateColor = function interpolateColor(percentage) {
+            var rgbVals = [];
+            for (var i = 0; i < 3; i++) {
+                var perc = percentage;
+                var top = opt.maxColor[i];
+                var bottom = opt.minColor[i];
+                var distance = top - bottom;
+                var position = bottom + perc / 100 * distance;
+                rgbVals.push(position);
+            }
+            var result = 'rgb(' + rgbVals[0] + ',' + rgbVals[1] + ',' + rgbVals[2] + ')';
+            return result;
+        };
 
-		//Execution
-		drawCircle();
-		drawLine(90 - angleIncr * 1, 90 - angleIncr * 4);
-		drawLine(90 - angleIncr * 1, 90 - angleIncr * 7);
-		drawLine(90 - angleIncr * 2, 90 - angleIncr * 4);
-		drawLine(90 - angleIncr * 2, 90 - angleIncr * 8);
-		drawLine(90 - angleIncr * 3, 90 - angleIncr * 6);
-		drawLine(90 - angleIncr * 3, 90 - angleIncr * 9);
-		drawLine(90 - angleIncr * 5, 90 - angleIncr * 7);
-		drawLine(90 - angleIncr * 5, 90 - angleIncr * 8);
-		drawLine(90 - angleIncr * 6, 90 - angleIncr * 9);
-		for (var i = 0; i < 9; i++) {
-			drawPoint(0 - i * angleIncr + 50, i + 1, settings.data[i]);
-		}
-	};
+        var pairs = [[1, 4], [1, 7], [2, 4], [2, 8], [3, 6], [3, 9], [5, 7], [5, 8], [6, 9]];
 
-	window.onresize = function (event) {
-		resize();
-		draw();
-	};
-	draw();
+        //Execution
+        drawCircle();
+
+        pairs.forEach(function (pair) {
+            drawLine(90 - angleIncr * pair[0], 90 - angleIncr * pair[1]);
+        });
+
+        for (var i = 0; i < 9; i++) {
+            drawPoint(0 - i * angleIncr + 50, i + 1, settings.data[i]);
+        }
+    };
+
+    window.onresize = function (event) {
+        resize();
+        draw();
+    };
+    draw();
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,111 +1,119 @@
 const enneagram = (settings) => {
-	const canvas = document.getElementById(settings.targetId);
-	const ctx = canvas.getContext('2d');
-	let canvasW = getCanvasW()
-  let opt = {
-  	radius: canvas.width / 3,
-	  maxColor: [255, 10, 10],
-	  minColor: [230,230,230],
-	  lineWidth: 2,
-	  lineColor: 'black',
-	  pointSize: 15,
-	  pointFontSize: 17,
-	  fontColor: 'black'
-  }
-	let center_x = canvas.width / 2;
-	let center_y = canvas.height / 2;
-	const angleIncr = 360 / 9;
-
-	// override if options are set
-	for (let key in settings.options) {
-    if (settings.options.hasOwnProperty(key)) {
-    	opt[key] = settings.options[key]
+    const canvas = document.getElementById(settings.targetId);
+    const ctx = canvas.getContext('2d');
+    let canvasW = getCanvasW()
+    let opt = {
+        radius: canvas.width / 3,
+        maxColor: [255, 10, 10],
+        minColor: [230, 230, 230],
+        lineWidth: 2,
+        lineColor: 'black',
+        pointSize: 15,
+        pointFontSize: 17,
+        fontColor: 'black'
     }
-	}
+    let center_x = canvas.width / 2;
+    let center_y = canvas.height / 2;
+    const angleIncr = 360 / 9;
 
-	function getCanvasW () {
-		canvas.style.width='100%';
-	  canvas.width  = canvas.offsetWidth;
-	  canvas.style.height= `${canvas.width}px`;
-	  canvas.height = canvas.offsetHeight;
-	  return canvas.width
-	}
+    // override if options are set
+    for (let key in settings.options) {
+        if (settings.options.hasOwnProperty(key)) {
+            opt[key] = settings.options[key]
+        }
+    }
 
-	const resize = () => {
-		canvasW = getCanvasW();
-	  opt.radius = canvasW / 3;
-		center_x = canvasW / 2;
-		center_y = canvasW / 2;
-	}
+    function getCanvasW() {
+        canvas.style.width = '100%';
+        canvas.width = canvas.offsetWidth;
+        canvas.style.height = `${canvas.width}px`;
+        canvas.height = canvas.offsetHeight;
+        return canvas.width
+    }
 
-	// draw
-	const draw = () => {
-		const drawCircle = () =>{
-			ctx.lineWidth = opt.lineWidth;
-	    ctx.beginPath();
-	    ctx.arc(center_x, center_y, opt.radius, 0, 2 * Math.PI);
-	    ctx.stroke();
-		}
+    const resize = () => {
+        canvasW = getCanvasW();
+        opt.radius = canvasW / 3;
+        center_x = canvasW / 2;
+        center_y = canvasW / 2;
+    }
 
-		const drawPoint = (angle,label,percentage) => {
-	    const x = center_x + opt.radius * Math.cos(-angle*Math.PI/180);
-	    const y = center_y + opt.radius * Math.sin(-angle*Math.PI/180);
+    // draw
+    const draw = () => {
+        const drawCircle = () => {
+            ctx.lineWidth = opt.lineWidth;
+            ctx.beginPath();
+            ctx.arc(center_x, center_y, opt.radius, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
 
-	    ctx.fillStyle = interpolateColor(percentage);
-	    ctx.beginPath();
-	    ctx.arc(x, y, opt.pointSize, 0, 2 * Math.PI);
-	    ctx.fill();
-			ctx.fillStyle = opt.fontColor;
-			ctx.font = `${opt.pointFontSize}px Arial`;
-	    ctx.fillText(label,x - opt.pointFontSize / 4,y + opt.pointFontSize / 3);
-		}
+        const drawPoint = (angle, label, percentage) => {
+            const x = center_x + opt.radius * Math.cos(-angle * Math.PI / 180);
+            const y = center_y + opt.radius * Math.sin(-angle * Math.PI / 180);
 
-		const drawLine = (angle1, angle2) => {
-			const x1 = center_x + opt.radius * Math.cos(-angle1*Math.PI/180);
-	    const y1 = center_y + opt.radius * Math.sin(-angle1*Math.PI/180);
-	    const x2 = center_x + opt.radius * Math.cos(-angle2*Math.PI/180);
-	    const y2 = center_y + opt.radius * Math.sin(-angle2*Math.PI/180);
-	    ctx.strokeStyle = opt.lineColor;
-			ctx.lineWidth = opt.lineWidth;
-	    ctx.beginPath();
-			ctx.moveTo(x1, y1);
-			ctx.lineTo(x2, y2);
-			ctx.stroke();
-		}
+            ctx.fillStyle = interpolateColor(percentage);
+            ctx.beginPath();
+            ctx.arc(x, y, opt.pointSize, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.fillStyle = opt.fontColor;
+            ctx.font = `${opt.pointFontSize}px Arial`;
+            ctx.fillText(label, x - opt.pointFontSize / 4, y + opt.pointFontSize / 3);
+        }
 
-		const interpolateColor = (percentage) => {
-			let rgbVals = []
-			for (let i = 0; i < 3; i++) {
-				const perc = percentage;
-				const top = opt.maxColor[i];
-				const bottom = opt.minColor[i];
-				const distance = top - bottom;
-				const position = bottom + ((perc / 100) * distance);
-				rgbVals.push(position)
-			}
-			const result = `rgb(${rgbVals[0]},${rgbVals[1]},${rgbVals[2]})`;
-			return result
-		}
+        const drawLine = (angle1, angle2) => {
+            const x1 = center_x + opt.radius * Math.cos(-angle1 * Math.PI / 180);
+            const y1 = center_y + opt.radius * Math.sin(-angle1 * Math.PI / 180);
+            const x2 = center_x + opt.radius * Math.cos(-angle2 * Math.PI / 180);
+            const y2 = center_y + opt.radius * Math.sin(-angle2 * Math.PI / 180);
+            ctx.strokeStyle = opt.lineColor;
+            ctx.lineWidth = opt.lineWidth;
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+        }
 
-		//Execution
-		drawCircle();
-		drawLine(90 - angleIncr * 1, 90 - angleIncr * 4)
-		drawLine(90 - angleIncr * 1, 90 - angleIncr * 7)
-		drawLine(90 - angleIncr * 2, 90 - angleIncr * 4)
-		drawLine(90 - angleIncr * 2, 90 - angleIncr * 8)
-		drawLine(90 - angleIncr * 3, 90 - angleIncr * 6)
-		drawLine(90 - angleIncr * 3, 90 - angleIncr * 9)
-		drawLine(90 - angleIncr * 5, 90 - angleIncr * 7)
-		drawLine(90 - angleIncr * 5, 90 - angleIncr * 8)
-		drawLine(90 - angleIncr * 6, 90 - angleIncr * 9)
-		for (let i = 0; i < 9; i++) {
-			drawPoint(0 - (i * (angleIncr)) + 50, i + 1, settings.data[i]);
-		}
-	}
+        const interpolateColor = (percentage) => {
+            let rgbVals = []
+            for (let i = 0; i < 3; i++) {
+                const perc = percentage;
+                const top = opt.maxColor[i];
+                const bottom = opt.minColor[i];
+                const distance = top - bottom;
+                const position = bottom + ((perc / 100) * distance);
+                rgbVals.push(position)
+            }
+            const result = `rgb(${rgbVals[0]},${rgbVals[1]},${rgbVals[2]})`;
+            return result
+        }
 
-	window.onresize = (event) => {
-		resize();
-		draw();
-	};
-	draw();
+        const pairs = [
+            [1, 4],
+            [1, 7],
+            [2, 4],
+            [2, 8],
+            [3, 6],
+            [3, 9],
+            [5, 7],
+            [5, 8],
+            [6, 9]
+        ]
+
+        //Execution
+        drawCircle();
+
+        pairs.forEach(pair => {
+            drawLine(90 - angleIncr * pair[0], 90 - angleIncr * pair[1])
+        });
+
+        for (let i = 0; i < 9; i++) {
+            drawPoint(0 - (i * (angleIncr)) + 50, i + 1, settings.data[i]);
+        }
+    }
+
+    window.onresize = (event) => {
+        resize();
+        draw();
+    };
+    draw();
 }
